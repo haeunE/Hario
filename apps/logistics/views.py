383ba177f919f5533789d899b341587c1ee5058b
Grace903 <hakyeong0@naver.com>
2024-12-30 04:33:59 +0900

    Add: 분석 결과 요약 추가

--- apps/logistics/views.py
+++ apps/logistics/views.py
@@ -18,7 +18,6 @@ logistics = Blueprint('logistics', __name__, template_folder='templates', static
 
 geojson_path = os.path.join('apps', 'logistics', '서울_자치구_경계_2017.geojson')
 
-
 @logistics.route('/')
 def logistics_page():
     csv_files_all = [
@@ -30,17 +29,67 @@ def logistics_page():
         ('apps/logistics/logi_after.csv', '코로나 이후 (2022.05 ~ 2023.12)')
     ]
     
-    pie_item_img, line_item_img, weekday_line_img, columns_item = logistics_graph(csv_files_all, csv_files_term)
+    columns_item = [
+    '가구/인테리어', '도서/음반', '디지털/가전', '생활/건강', 
+    '스포츠/레저', '식품', '출산/육아', '패션의류', '패션잡화', '화장품/미용'
+    ]
+    
+    pie_item_img, line_item_img, weekday_line_img, columns_item = logistics_graph(csv_files_all, csv_files_term, columns_item)
     bar_item_img = logistics_bar_graph(csv_files_term)
     
-    dual_map = color_map(csv_files_term, columns_item )
+    df_all = read_csv('apps/logistics/logi_all.csv')
+    total_volume_all = df_all[columns_item].sum()  # 전체 품목별 운송량 합계
+
+    # pie_analysis        
+    top_pie_item, top_pie_percent, second_pie_item, second_pie_percent, third_pie_item, third_pie_percent = pie_analysis(total_volume_all)
+   
+    # bar_analysis        
+    high_bar_period, high_bar_volume, low_bar_period, low_bar_volume = bar_analysis(csv_files_term, columns_item)
     
-    return render_template('logistics/logistics.html', 
-                           pie_plot_urls=pie_item_img, 
-                           line_plot_urls=line_item_img, 
-                           weekday_line_plot_urls=weekday_line_img,
-                           bar_plot_urls=bar_item_img,
-                           dual_map=dual_map)
+    # map_analysis        
+    dual_map, top_sender, top_receiver, combined_text = color_map(csv_files_term, columns_item)
+    
+    # line_analysis        
+    high_month, low_month, monthly_avg_volume = line_analysis(csv_files_term, columns_item)
+
+    # weekday_line_analysis
+    weekday_analysis_result = weekday_line_analysis(csv_files_all, columns_item)
+
+
+    return render_template(
+        'logistics/logistics.html', 
+        pie_plot_urls=pie_item_img, 
+        line_plot_urls=line_item_img, 
+        bar_plot_urls=bar_item_img,
+        weekday_line_plot_urls=weekday_line_img,
+        
+        dual_map=dual_map, 
+        top_sender=top_sender, 
+        top_receiver=top_receiver, 
+        combined_text=combined_text,
+        
+        top_pie_item=top_pie_item,
+        top_pie_percent=top_pie_percent,
+        second_pie_item=second_pie_item,
+        second_pie_percent=second_pie_percent,
+        third_pie_item=third_pie_item,
+        third_pie_percent=third_pie_percent,
+        
+        high_bar_period=high_bar_period,
+        high_bar_volume=high_bar_volume,
+        low_bar_period=low_bar_period,
+        low_bar_volume=low_bar_volume,
+        
+        high_month=high_month,
+        low_month=low_month,
+        monthly_avg_volume=monthly_avg_volume,
+        total_volume_all=total_volume_all,
+
+        high_day=weekday_analysis_result['high_day'],
+        high_day_mean=weekday_analysis_result['high_day_mean'],
+        low_day=weekday_analysis_result['low_day'],
+        low_day_mean=weekday_analysis_result['low_day_mean']
+    )
 
 
 def read_csv(file_path):
@@ -50,12 +99,8 @@ def read_csv(file_path):
     return pd.read_csv(file_path, encoding=encoding) 
 
 
-def logistics_graph(csv_files_all, csv_files_term):
+def logistics_graph(csv_files_all, csv_files_term, columns_item):
     pie_item_img, line_item_img, weekday_line_img = [], [], []
-    columns_item = [
-        '가구/인테리어', '도서/음반', '디지털/가전', '생활/건강', 
-        '스포츠/레저', '식품', '출산/육아', '패션의류', '패션잡화', '화장품/미용'
-    ]
 
     # 전체 기간 그래프 생성 (csv_files_all)
     for file_path, title_prefix in csv_files_all:
@@ -105,7 +150,7 @@ def plot_pie_graph(data, title):
     
     plt.legend(wedges, sorted_data.index, loc='upper left', bbox_to_anchor=(1, 1), fontsize=10)
     plt.tight_layout()
-    plt.title(f'{title}')
+    # plt.title(f'{title}')
 
     pie_img_io = io.BytesIO()
     plt.savefig(pie_img_io, format='png')
@@ -114,6 +159,17 @@ def plot_pie_graph(data, title):
     plt.close()
 
     return pie_item_img    
+
+
+def pie_analysis(total_volume):
+    total = total_volume.sum()
+    sorted_volume = total_volume.sort_values(ascending=False)
+    top_items = sorted_volume.iloc[:3]
+    top_items_percentage = (top_items / total) * 100
+
+    result = [(top_items.index[i], round(top_items_percentage.iloc[i], 1)) for i in range(3)]
+
+    return tuple(result[0] + result[1] + result[2])
 
 
 # 코로나 기간 별 운송량 (막대 그래프)
@@ -153,6 +209,23 @@ def plot_bar_graph(title, columns_item, total_volume):
     return bar_item_img
 
 
+def bar_analysis(csv_files_term, columns_item):
+    total_volume = pd.DataFrame()
+    
+    for file_path, title_prefix in csv_files_term:
+        df = read_csv(file_path)
+        df_selected = df[columns_item]
+        total_volume[title_prefix] = df_selected.sum()
+    
+    # 각 기간별 총 운송량과 평균 구하기
+    high_bar_period = total_volume.sum(axis=0).idxmax()
+    high_bar_volume = total_volume[high_bar_period].mean()
+    low_bar_period = total_volume.sum(axis=0).idxmin()
+    low_bar_volume = total_volume[low_bar_period].mean()
+    
+    return high_bar_period, high_bar_volume, low_bar_period, low_bar_volume
+
+
 def plot_line_graph(df, columns_item, title):
     df_monthly = df[columns_item].resample('ME').sum()
     fig, ax = plt.subplots(figsize=(17, 4))
@@ -176,6 +249,36 @@ def plot_line_graph(df, columns_item, title):
     return line_item_img
 
 
+def line_analysis(csv_files_term, columns_item):
+    monthly_avg_volume = {}  # 월별 평균 운송량
+    total_avg_volume = []    # 전체 운송량 리스트
+
+    for file_path, title_prefix in csv_files_term:
+        df = read_csv(file_path)
+        df['배송년월일'] = pd.to_datetime(df['배송년월일'], format='%Y%m%d')
+        df.set_index('배송년월일', inplace=True)
+
+        # 월별 데이터 집계
+        df_monthly = df[columns_item].resample('M').sum()
+        total_avg_volume.extend(df_monthly.sum(axis=1).values)
+
+    total_avg_volume = pd.Series(total_avg_volume, index=pd.date_range(start=df_monthly.index[0], periods=len(total_avg_volume), freq='M'))
+    monthly_avg_volume = total_avg_volume.mean() # 월별 평균 운송량
+
+    max_month = total_avg_volume.idxmax()
+    min_month = total_avg_volume.idxmin()
+
+    high_month = (max_month.year, max_month.month, total_avg_volume[max_month])
+    low_month = (min_month.year, min_month.month, total_avg_volume[min_month])
+    
+    # 결과 형식 지정
+    high_month = (max_month.year, max_month.month, f"{total_avg_volume[max_month]:,}")
+    low_month = (min_month.year, min_month.month, f"{total_avg_volume[min_month]:,}")
+    monthly_avg_volume = f"{monthly_avg_volume:,.0f}"
+
+    return high_month, low_month, monthly_avg_volume
+
+
 def plot_weekday_line_graph(csv_files_all, columns_item):
     df = read_csv(csv_files_all)
     df['배송년월일'] = pd.to_datetime(df['배송년월일'], format='%Y%m%d')
@@ -184,7 +287,7 @@ def plot_weekday_line_graph(csv_files_all, columns_item):
     df['요일'] = df.index.dayofweek  # 요일 계산
     df_weekday = df[df['요일'] < 5]  # 주말 필터링
     
-    weekday_sum = df_weekday.groupby('요일')[columns_item].sum()  # 요일별 운송량 집계
+    weekday_sum = df_weekday.groupby('요일')[columns_item].sum()
     
     fig, ax = plt.subplots(figsize=(7, 4))
     weekday_sum.plot(ax=ax)
@@ -213,7 +316,29 @@ def plot_weekday_line_graph(csv_files_all, columns_item):
     return weekday_volume
 
 
-# 여기서부터 듀얼맵
+def weekday_line_analysis(csv_files_all, columns_item):
+    df = pd.concat([read_csv(file_path) for file_path, _ in csv_files_all], ignore_index=True)
+    df['배송년월일'] = pd.to_datetime(df['배송년월일'], format='%Y%m%d')
+    df.set_index('배송년월일', inplace=True)
+    
+    # 요일별로 운송량 합계 계산 (주말 제외)
+    df['요일'] = df.index.dayofweek
+    df_weekday = df[df['요일'] < 5]  # 주말 필터링 (0:월~4:금)
+    weekday_sum = df_weekday.groupby('요일')[columns_item].sum()
+    weekday_avg = weekday_sum.mean(axis=1)
+
+    high_day, low_day = weekday_avg.idxmax(), weekday_avg.idxmin()
+    
+    days_of_week = ['월', '화', '수', '목', '금']
+    return {
+        'high_day': days_of_week[high_day],
+        'high_day_mean': round(weekday_avg.max(), 2),
+        'low_day': days_of_week[low_day],
+        'low_day_mean': round(weekday_avg.min(), 2),
+    }
+
+
+# 여기부터 듀얼맵
 
 def color_map(csv_files_term, columns_item):  # 운송량 집계, 색상 매핑
     df = pd.concat([read_csv(file) for file, _ in csv_files_term], ignore_index=True)
@@ -238,11 +363,6 @@ def color_map(csv_files_term, columns_item):  # 운송량 집계, 색상 매핑
     sender_rank = gu_rank('송하인_구명') 
     receiver_rank = gu_rank('수하인_구명') 
     dual_map = generate_dual_map(sender_rank, receiver_rank)
-
-    print('================')
-    print(sender_rank)
-    print(receiver_rank)
-    print('================')
 
     return dual_map
 
@@ -319,23 +439,25 @@ def generate_dual_map(sender_rank, receiver_rank):
     sender_geojson()
     receiver_geojson()
 
-    # 지도 요약 - 지역 별 수요 top 3
-    def map_summary():
-        top_sender = sender_rank.nlargest(3, '운송량')[['송하인_구명', '운송량']]
-        top_receiver = receiver_rank.nlargest(3, '운송량')[['수하인_구명', '운송량']]
-        all_volume = sender_rank.copy()
-        all_volume['송수하인_합계'] = sender_rank['운송량'] + receiver_rank['운송량']
-        top_combined = all_volume.nlargest(3, '송수하인_합계')[['송하인_구명', '송수하인_합계']]
-
-        sender_text = "\n".join([f"{i+1}위: {row['송하인_구명']} ({row['운송량']} 건)" for i, row in top_sender.iterrows()])
-        receiver_text = "\n".join([f"{i+1}위: {row['수하인_구명']} ({row['운송량']} 건)" for i, row in top_receiver.iterrows()])
-        combined_text = "\n".join([f"{i+1}위: {row['송하인_구명']} ({row['송수하인_합계']} 건)" for i, row in top_combined.iterrows()])
-
-        return sender_text, receiver_text, combined_text
-
-    sender_text, receiver_text, combined_text = map_summary()
+    sender_text, receiver_text, combined_text = map_analysis(sender_rank, receiver_rank)
 
     dual_map_file_path = os.path.join('apps/logistics/static/images', 'dual_map.html')
     m.save(dual_map_file_path)
 
     return dual_map_file_path, sender_text, receiver_text, combined_text
+
+
+# 지도 요약 - 지역 별 수요 top 3
+def map_analysis(sender_rank, receiver_rank):
+    
+    all_volume = sender_rank.copy()
+    all_volume['송수하인_합계'] = all_volume['운송량'] + receiver_rank['운송량']
+    
+    top_sender = sender_rank.nlargest(1, '운송량')['송하인_구명'].iloc[0]
+    top_receiver = receiver_rank.nlargest(1, '운송량')['수하인_구명'].iloc[0]
+    
+    # 송수하인 합계 순위
+    top_combined = all_volume.nlargest(3, '송수하인_합계')[['송하인_구명', '송수하인_합계']]
+    combined_text = " - ".join([f"{row['송하인_구명']} ({row['송수하인_합계']} 건)" for _, row in top_combined.iterrows()])
+
+    return top_sender, top_receiver, combined_text
